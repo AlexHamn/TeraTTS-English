5d766c89eb25f9e2843f9d6a37a32be4dbcf29cf
Alex H <agalexaandro16@gmial.com>
2024-03-22 19:19:10 +0100

    translate comments in example.py file

--- example.py
+++ example.py
@@ -1,28 +1,29 @@
-text = "Привет, мир!"
+text = "Дамы и господа, началась посадка на рейс Alex-69. Пожалуйста пройдите на гейт 69."
 
 from TeraTTS import TTS
 
-# Опционально: Предобработка текста (улучшает качество)
+# Optional: Text preprocessing (improves quality)
 from ruaccent import RUAccent
 accentizer = RUAccent()
 
-# Загрузка моделей акцентуации и словарей
+# Loading accentuation models and dictionaries
 accentizer.load(omograph_model_size='turbo', use_dictionary=True)
 
-# Обработка текста с учетом ударений и буквы ё
+# Processing text with stress marks and the letter ё
 text = accentizer.process_all(text)
-print(f"Текст с ударениями и ё: {text}")
+print(f"Text with stress marks and ё: {text}")
 
+# Note: You can find all models at https://huggingface.co/TeraTTS, including the GLADOS model
+tts = TTS("TeraTTS/natasha-g2p-vits", add_time_to_end=1.0, tokenizer_load_dict=True) 
+# You can adjust 'add_time_to_end' for audio duration, 'tokenizer_load_dict' can be disabled if using RUAccent
 
-# Примечание: Вы можете найти все модели по адресу https://huggingface.co/TeraTTS, включая модель GLADOS
-tts = TTS("TeraTTS/natasha-g2p-vits", add_time_to_end=1.0, tokenizer_load_dict=True) # Вы можете настроить 'add_time_to_end' для продолжительности аудио, 'tokenizer_load_dict' можно отключить если используете RUAccent
+# 'length_scale' can be used to slow down the audio for better sound (default 1.1, specified here for example)
+audio = tts(text, length_scale=1.1)  
+# Create audio. You can add stress marks using '+'
+tts.play_audio(audio)  
+# Play the created audio
+tts.save_wav(audio, "./test.wav")  
+# Save the audio to a file
 
-
-# 'length_scale' можно использовать для замедления аудио для лучшего звучания (по умолчанию 1.1, указано здесь для примера)
-audio = tts(text, lenght_scale=1.1)  # Создать аудио. Можно добавить ударения, используя '+'
-tts.play_audio(audio)  # Воспроизвести созданное аудио
-tts.save_wav(audio, "./test.wav")  # Сохранить аудио в файл
-
-
-# Создать аудио и сразу его воспроизвести
-tts(text, play=True, lenght_scale=1.1)
+# Create audio and play it immediately
+tts(text, play=True, length_scale=1.1)
